71deee70c72f4148db250e253dbdd1113123a6d1
[carlos martinez] <theycallhellp@gmail.com>
2022-10-27 00:04:16 -0700

    api put

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -39,7 +39,7 @@ const saveNote = (note) =>
     headers: {
       'Content-Type': 'application/json',
     },
-    body: note,
+    body: JSON.stringify(note),
   });
 
 const deleteNote = (id) =>
@@ -122,7 +122,7 @@ const renderNoteList = async (notes) => {
   if (window.location.pathname === '/notes') {
     noteList.forEach((el) => (el.innerHTML = ''));
   }
-
+  console.log('test');
   let noteListItems = [];
 
   // Returns HTML element with or without a delete button
--- server.js
+++ server.js
@@ -1,9 +1,14 @@
 const express = require('express');
 const fs = require('fs');
+const db = require('./db/db.json')
 const { v4: uuidv4 } = require('uuid');
 const port = process.env.PORT || 3000;
 
 const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 
 app.use(express.static(__dirname + '/public'));
 
@@ -12,21 +17,20 @@ app.get('/notes', (req, res) => {
 })
 
 app.get('/api/notes', (req, res) => {
-    res.json(fs.readFileSync(__dirname + '/db/db.json'));
+    let currentNotes = JSON.parse(fs.readFileSync(__dirname + '/db/db.json'));
+    console.log(currentNotes);
+    res.json(currentNotes);
 })
 
 app.post('/api/notes', (req, res) => {
+    console.log('BODY', req.body);
     let note = req.body;
     console.log(note);
     note.id = uuidv4();
-    let currentNote = JSON.parse(fs.readFileSync(__dirname + '/db/db.json'));
-    currentNote.push(note);
-    fs.writeFile(`./db/db.json`, JSON.stringify(currentNote, null, '\t'), (err) => err ? console.error(err) : console.log(`Note for ${note.title} has been written to JSON file`));
-    const response = {
-        status: 'added',
-        body: note
-    };
-    res.json(response);
+    let currentNotes = JSON.parse(fs.readFileSync(__dirname + '/db/db.json'));
+    currentNotes.push(note);
+    fs.writeFile(`./db/db.json`, JSON.stringify(currentNotes, null, '\t'), (err) => err ? console.error(err) : console.log(`Note for ${note.title} has been written to JSON file`));
+    res.send('Success');
 })
 
 app.get('/*', (req, res) => {
